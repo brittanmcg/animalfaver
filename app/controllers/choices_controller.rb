7df4667b53dfbc46dfc4915550e1a00af7058a65
Brittan McGinnis <konamax123@gmail.com>
2014-08-04 13:26:40 -0700

    able to create a new choice

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,11 +1,16 @@
 class ChoicesController < ApplicationController
 
   def index
-    # @choices = Choice.all 
+    @choices = Choice.all 
   end
 
   def new
-    
+    @choice = Choice.new
+  end
+
+  def create
+    @choice = Choice.create(query: params[:choice][:query])
+    p @choice
   end
 
 end
